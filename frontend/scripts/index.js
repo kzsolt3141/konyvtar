02417168de7f2c27bf87507c6326fcc3e9c7977d
Kovacs Zsolt <kzsolt3141@yahoo.com>
2023-11-20 00:56:12 +0200

    Kliens kereses WIP

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,11 +1,11 @@
-const form = document.getElementById("konyv_kereses");
-var listaDiv = document.querySelector(".lista");
+const konyvForm = document.getElementById("konyv_kereses");
+var listaDiv = document.querySelector(".konyvlista");
 
-form.addEventListener("submit", async (event) => {
+konyvForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   listaDiv.innerHTML = "";
 
-  const formData = new FormData(form);
+  const formData = new FormData(konyvForm);
 
   formData.forEach((value, key) => {
     console.log(key + ": " + value);
@@ -58,3 +58,64 @@ function konyv_torles(key) {
     })
   );
 }
+
+// const kliensForm = document.getElementById("kliens_kereses");
+// var listaDiv = document.querySelector(".klienslista");
+
+// kliensForm.addEventListener("submit", async (event) => {
+//   event.preventDefault();
+//   listaDiv.innerHTML = "";
+
+//   const formData = new FormData(kliensForm);
+
+//   formData.forEach((value, key) => {
+//     console.log(key + ": " + value);
+//   });
+
+//   fetch("/konyv/kereses", {
+//     method: "POST",
+//     body: formData,
+//   })
+//     .then((rsp) => rsp.json())
+//     .then((data) => {
+//       JSON.parse(data).forEach((value, key) => {
+//         console.log(value);
+//         if (listaDiv) {
+//           var paragraph = document.createElement("p");
+//           paragraph.textContent = value.konyv_cim + " " + value.szerzo;
+//           listaDiv.appendChild(paragraph);
+//           var button = document.createElement("button");
+//           button.textContent = "Torles";
+//           button.addEventListener("click", function () {
+//             konyv_torles(value.isbn);
+//           });
+//           listaDiv.appendChild(button);
+//           button = document.createElement("button");
+//           button.textContent = "Szerkesztes";
+//           button.addEventListener("click", function () {
+//             konyv_torles(value.isbn);
+//           });
+//           listaDiv.appendChild(button);
+//           button = document.createElement("button");
+//           button.textContent = "Reszletek";
+//           button.addEventListener("click", function () {
+//             konyv_torles(value.isbn);
+//           });
+//           listaDiv.appendChild(button);
+//         }
+//         // add to the page
+//       });
+//     });
+// });
+
+// function konyv_torles(key) {
+//   fetch("/konyv/torles", {
+//     method: "POST",
+//     body: key,
+//   }).then((rsp) =>
+//     rsp.json().then((data) => {
+//       console.log(data);
+//       //TODO @Zsolt refresh the list of books
+//     })
+//   );
+// }
