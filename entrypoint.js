f6922cc4d1fbba7e2304fcdb03f1d6552759cb4a
Kovacs Zsolt <kzsolt3141@yahoo.com>
2024-01-14 23:03:53 +0200

    add timestamp to IP log

--- entrypoint.js
+++ entrypoint.js
@@ -20,13 +20,25 @@ const userRoute = require("./backend/routes/User");
 const loanRoute = require("./backend/routes/Loan");
 
 app.use((req, res, next) => {
+  const timeStamp = Date.now();
+  const date = new Date(timeStamp);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-based, so add 1 and pad with '0'
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
   const clientIp =
     req.headers["x-forwarded-for"] ||
     req.headers["x-real-ip"] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
-  console.log(clientIp);
+  console.log(
+    `${year}-${month}-${day}; ${hours}:${minutes}:${seconds} => ` + clientIp
+  );
   next();
 });
 
