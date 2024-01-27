159f462fb7cf917bd560c16f3812462a11cb41e3
Kovacs Zsolt <kzsolt3141@yahoo.com>
2024-01-28 00:20:31 +0200

    frontend -> public

--- entrypoint.js
+++ entrypoint.js
@@ -5,15 +5,15 @@ const ejs = require("ejs");
 const session = require("express-session");
 const flash = require("express-flash");
 
-const database = require("./backend/db_common.js");
+const common = require("./backend/common.js");
 
 //----------------------------------------------------------------
-database.init();
+common.db_init();
 
 //----------------------------------------------------------------
 const app = express();
 
-app.use(express.static(path.join(process.cwd(), "frontend")));
+app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.static(path.join(process.cwd(), "uploads")));
 
 app.set("view engine", "ejs");
@@ -41,28 +41,7 @@ const bookRoute = require("./backend/routes/Book");
 const userRoute = require("./backend/routes/User");
 const loanRoute = require("./backend/routes/Loan");
 
-app.use((req, res, next) => {
-  const timeStamp = Date.now();
-  const date = new Date(timeStamp);
-
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-based, so add 1 and pad with '0'
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-
-  const clientIp =
-    req.headers["x-forwarded-for"] ||
-    req.headers["x-real-ip"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-  console.log(
-    `${year}-${month}-${day}; ${hours}:${minutes}:${seconds} => ` + clientIp
-  );
-  next();
-});
+app.use(common.ipLoggerMW);
 
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
@@ -79,5 +58,5 @@ app.listen(8080, () => {
 });
 
 app.get("/backup", (req, res) => {
-  database.saveAllTables(res);
+  common.saveAllTables(res);
 });
