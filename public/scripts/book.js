1bd1ad77d7ef3516bc2dd92d4eadac598c500fb1
Kovacs Zsolt <kzsolt3141@yahoo.com>
2024-01-30 22:44:26 +0200

    update book editing route, show book thubnail at editing

--- public/scripts/book.js
+++ public/scripts/book.js
@@ -3,7 +3,3 @@ import { fillGenreSelect } from "./genre.js";
 
 //create genre selection drop-down
 fillGenreSelect(document.getElementById("genre"));
-
-//TODO check BID and NEWBOOK
-// if book is new, do not change
-// if book is to be edited, update fields + form URL
