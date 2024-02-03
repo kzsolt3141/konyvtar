cf7a38583f1f2e8b6a19a3b858b416e0ad1eb5b8
Kovacs Zsolt <kzsolt3141@yahoo.com>
2024-02-03 12:56:33 +0200

    remove full book page, integrate functionalyty in book page

--- public/scripts/book.js
+++ public/scripts/book.js
@@ -1,5 +1,6 @@
 //import genre related functions
 import { fillGenreSelect } from "./genre.js";
+import { common } from "./common.js";
 
 //create genre selection drop-down
 fillGenreSelect(document.getElementById("genre"));
@@ -18,3 +19,30 @@ document.getElementById("image").addEventListener("change", function (event) {
     reader.readAsDataURL(fileInput.files[0]);
   }
 });
+
+const element = document.getElementById("bid");
+if (element) {
+  const bid = element.getAttribute("content");
+  if (bid != "") {
+    const bookNotes = await common.getBookNotesById(bid);
+    const loans = await common.getLoanById(bid);
+
+    const bookNotesText = document.getElementById("book_notes");
+    bookNotes.forEach((bookNote) => {
+      for (const k in bookNote) {
+        const e = document.createElement("p");
+        e.textContent = bookNote[k];
+        bookNotesText.appendChild(e);
+      }
+    });
+
+    const bookLoanText = document.getElementById("book_loan");
+    loans.forEach((loan) => {
+      for (const k in loan) {
+        const e = document.createElement("p");
+        e.textContent = loan[k];
+        bookLoanText.appendChild(e);
+      }
+    });
+  }
+}
