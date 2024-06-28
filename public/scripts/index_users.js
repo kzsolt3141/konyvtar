85736deae53ef4c60b3fe50bb602391b6368a7e6
kzsolt3141 <kzsolt3141@gmail.com>
2024-06-29 00:57:49 +0300

    Fix when clicked to the user icon

--- public/scripts/index_users.js
+++ public/scripts/index_users.js
@@ -24,7 +24,7 @@ if (userSearchBtn) {
 
 function userClickCb(id) {
   common.disableMain();
-  window.location.href = `/user/full/${id}`;
+  window.location.href = `/user/${id}`;
   common.enableMain();
 }
 
